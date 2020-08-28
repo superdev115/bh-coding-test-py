8ac4baa4b53903a532b7bf282a5931c9f4404458
Zemin Li <lizemin220@gmail.com>
2020-08-28 15:42:48 +0800

    add routes/navigations

--- project/controllers/main.py
+++ project/controllers/main.py
@@ -3,5 +3,18 @@ from project import app
 from flask import render_template
 
 @app.route('/')
-def start():
-    return render_template('main/index.html')
+@app.route('/load_products')
+def load_products():
+    return render_template('main/load_products.html')
+
+@app.route('/add_product')
+def add_product():
+    return render_template('main/add_product.html')
+
+@app.route('/list_products')
+def list_products():
+    return render_template('main/list_products.html')
+
+@app.route('/email_schedule')
+def email_schedule():
+    return render_template('main/email_schedule.html')
